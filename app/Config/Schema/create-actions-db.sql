6cf5f7a8d2a1ec82cc564c3462bae43e1c128216
Chris Millar <chaddock@ps109753.dreamhostps.com>
2012-10-17 10:15:26 -0700

    Updated SQL dump file

--- app/Config/Schema/create-actions-db.sql
+++ app/Config/Schema/create-actions-db.sql
@@ -1,4 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.10.4
+-- http://www.phpmyadmin.net
+--
+-- Host: mysql.izumit.com
+-- Generation Time: Oct 17, 2012 at 10:13 AM
+-- Server version: 5.1.53
+-- PHP Version: 5.2.17
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `actionsgtd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actions`
+--
 
 DROP TABLE IF EXISTS `actions`;
 CREATE TABLE IF NOT EXISTS `actions` (
@@ -9,9 +28,16 @@ CREATE TABLE IF NOT EXISTS `actions` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `project_id` int(11) unsigned DEFAULT NULL,
+  `duedate` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `project_id` (`project_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
 
 DROP TABLE IF EXISTS `projects`;
 CREATE TABLE IF NOT EXISTS `projects` (
@@ -21,7 +47,14 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `actions`
+--
 ALTER TABLE `actions`
   ADD CONSTRAINT `project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
